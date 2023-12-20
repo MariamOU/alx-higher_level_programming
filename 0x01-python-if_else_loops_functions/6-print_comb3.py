0a0f4e3c1946f21c79540b2d0470cdbc5ce8d8ed
MariamOU <marymyara@gmail.com>
2023-12-19 17:34:20 -0800

    7-islower.py

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 print("{:02d}".format(1), end='')
 for i in range(2, 10):
-    for j in range(i, 10):
-        print(", {:02d}".format(i * 10 + j), end='')
-print()
+    for j in range(i + 1, 10):
+        if i == j:
+            break
+        else:
+            print(", {:02d}".format(i + j))
