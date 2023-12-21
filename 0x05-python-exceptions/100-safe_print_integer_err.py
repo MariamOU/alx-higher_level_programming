01c09759fd1664dc62d7591ff523e0bab3126349
MariamOU <marymyara@gmail.com>
2023-12-20 16:21:37 -0800

    100-safe_print_integer_err.py

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    try:
+
+        print("{:d}".format(int(value)))
+        return True
+    except ValueError as e:
+        sys.stderr.write(f"Exception: {e}\n")
+        return False
