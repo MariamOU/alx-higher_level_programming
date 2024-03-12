fc238f894ccdd97825c84cbb4cf26b859e6bc4c3
MariamOU <marymyara@gmail.com>
2024-03-12 00:33:38 -0700

    9-cities_by_state_join.sql modif

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,3 @@
 -- lists all cities contained in the database hbtn_0d_usa.
 SELECT id, name FROM cities, name FROM states ORDER BY id ASC;
+SELECT cities.id, cities.name (SELECT states.name FROM states WHERE states.id = cities.state_id) FROM cities ORDER BY cities.id ASC;
