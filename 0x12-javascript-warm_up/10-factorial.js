27b64988f67d1b4a8a29a79f37d3c6dddb9212a7
MariamOU <marymyara@gmail.com>
2024-03-15 01:56:37 -0700

    10-factorial.js modif

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,10 +2,10 @@
 function factorial (a) {
   if (a < 0) {
     return (-1);
-    }
+  }
   else if (a === 0 || isNaN(a)) {
     return (1);
-    }
+  }
   else {
     return (a * factorial(a - 1));
   }
