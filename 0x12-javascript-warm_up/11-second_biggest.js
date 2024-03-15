44ad117d2d7ea0865609c10799cb287ae09f5a2f
MariamOU <marymyara@gmail.com>
2024-03-15 01:25:06 -0700

    files 9, 10, 11, 12, 13

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  const arg = process.argv.slice(2).map(parseInt);
+  const argg = arg.sort(function(a, b) { return (b - a)})[1];
+  console.log(argg);
+}
