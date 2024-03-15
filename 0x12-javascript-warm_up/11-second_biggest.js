c651bcf9e32b56f403552fecfb1591e96cb41bee
MariamOU <marymyara@gmail.com>
2024-03-15 02:16:09 -0700

    11-second_biggest.js modif

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 3) {
   console.log('0');
 } else {
-  const arg = process.argv.slice(2).map(parseInt);
+  const arg = process.argv.slice(2).map(Number);
   const argg = arg.sort(function (a, b) { return b - a; })[1];
   console.log(argg);
 }
