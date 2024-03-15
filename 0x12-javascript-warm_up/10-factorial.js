deb4aac0275d52c1b7d4af43e4662fe69f7c50a1
MariamOU <marymyara@gmail.com>
2024-03-15 01:51:01 -0700

    10-factorial.js modif

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,14 @@
 #!/usr/bin/node
-function factorial(a) {
+function factorial (a) {
   if (a < 0) {
-  return (-1);
-  }
+    return (-1);
+    }
   else if (a === 0 || isNaN(a)) {
-  return (1);
-  }
+    return (1);
+    }
   else
   {
-  return (a * factorial(a - 1));
-   }
+    return (a * factorial(a - 1));
+  }
 }
 console.log(factorial(parseInt(process.argv[2])));
