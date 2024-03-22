99551878633c38171f5ff8e953be729cc55714d2
MariamOU <marymyara@gmail.com>
2024-03-22 08:25:19 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
-    number_arguments = len(argv) - 1
-    if number_arguments == 0:
-        print("0 arguments.")
-        print(".")
-    else:
-        print(f"{number_arguments} argument{'s' if number_arguments > 1 else ''}:")
-        for i in range(1, len(argv)):
-            print(f"{i}: {argv[i]}")
+from sys import argv
+number_arguments = len(argv) - 1
+if number_arguments == 0:
+    print("0 arguments.")
+else:
+    print(f"{number_arguments} argument{'s' if number_arguments > 1 else ''}:")
+    for i in range(1, len(argv)):
+        print(f"{i}: {argv[i]}")
