d17a74d1af32d209072bc0a054a7121588b10f16
MariamOU <marymyara@gmail.com>
2024-03-22 08:23:07 -0700

    2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,6 @@
-from sys import argv
-
+#!/usr/bin/python3
 if __name__ == "__main__":
+    from sys import argv
     number_arguments = len(argv) - 1
     if number_arguments == 0:
         print("0 arguments.")
