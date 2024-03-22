e3e4ac2e9437c45c77a9b67cf33b9f74fa8e15ff
MariamOU <marymyara@gmail.com>
2024-03-22 08:54:39 -0700

    3-infinite_add.py

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-   from sys import argv
-   add_args = 0
-   for i in range(1, len(argv)):
+    from sys import argv
+    add_args = 0
+    for i in range(1, len(argv)):
        add_args += int(argv[i])
-       print("{}".format(add_args))
+    print("{}".format(add_args))
