1a4ddd8be592feb1b556d80b6c5527c8b110520f
MariamOU <marymyara@gmail.com>
2024-03-22 09:13:40 -0700

    5-variable_load.py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import importlib
-"""Imports a variable and prints its value"""
 if __name__ == "__main__":
-    module = importlib.import_module('variable_load_5')
-    print(module.a)
+    from variable_load_5 import a
+    print(f"{a}")
